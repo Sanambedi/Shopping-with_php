1a5f973b4ff739d1fa3ee7894101d018272d8683
Sanambedi <55624366+Sanambedi@users.noreply.github.com>
2021-04-09 23:55:43 +0530

    origin

--- web/index.php
+++ web/index.php
@@ -10,9 +10,6 @@
 	/>
 	<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 	<?php
-	if(isset($_POST["show-item"])){
-		header("location: ./single.php");
-	}
 	include "header.php"?>
 	<!-- banner -->
 	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
