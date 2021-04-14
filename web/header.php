a19ea1ce2319b1d2b0a0d171344133dfc3067053
Sanambedi <55624366+Sanambedi@users.noreply.github.com>
2021-04-14 23:34:49 +0530

    origin

--- web/header.php
+++ web/header.php
@@ -80,12 +80,29 @@
 			}
 			
 		}
+		$('.toast').toast(option)
 	</script>
 	</head>
 
 <body>
 <!-- top-header -->
 	<!-- Top Navbar starts-->
+	<div aria-live="polite" aria-atomic="true" class="d-flex justify-content-center align-items-center" style="min-height: 200px;">
+  		<!-- Then put toasts within -->
+  		<div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    		<div class="toast-header">
+      			<img src="..." class="rounded mr-2" alt="...">
+      			<strong class="mr-auto">Bootstrap</strong>
+      			<small>11 mins ago</small>
+      			<button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+        			<span aria-hidden="true">&times;</span>
+      			</button>
+    		</div>
+    		<div class="toast-body">
+      			Hello, world! This is a toast message.
+    		</div>
+  		</div>
+	</div>
 	<div class="agile-main-top">
 		<div class="container-fluid">
 			<div class="row main-top-w3l py-2">
