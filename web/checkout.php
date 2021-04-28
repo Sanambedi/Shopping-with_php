10c562caa00e23aa065da012a0d769f43e477a87
Sanambedi <55624366+Sanambedi@users.noreply.github.com>
2021-04-28 23:41:22 +0530

    origin

--- web/checkout.php
+++ web/checkout.php
@@ -171,51 +171,10 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 					</table>
 				</div>
 			</div>
-			<div class="checkout-left">
-				<div class="address_form_agile mt-sm-5 mt-4">
-					<h4 class="mb-sm-4 mb-3">Add a new Details</h4>
-					<form action="payment.php" method="post" class="creditly-card-form agileinfo_form">
-						<div class="creditly-wrapper wthree, w3_agileits_wrapper">
-							<div class="information-wrapper">
-								<div class="first-row">
-									<div class="controls form-group">
-										<input class="billing-address-name form-control" type="text" name="name" placeholder="Full Name" required="">
-									</div>
-									<div class="w3_agileits_card_number_grids">
-										<div class="w3_agileits_card_number_grid_left form-group">
-											<div class="controls">
-												<input type="text" class="form-control" placeholder="Mobile Number" name="number" required="">
-											</div>
-										</div>
-										<div class="w3_agileits_card_number_grid_right form-group">
-											<div class="controls">
-												<input type="text" class="form-control" placeholder="Landmark" name="landmark" required="">
-											</div>
-										</div>
-									</div>
-									<div class="controls form-group">
-										<input type="text" class="form-control" placeholder="Town/City" name="city" required="">
-									</div>
-									<div class="controls form-group">
-										<select class="option-w3ls">
-											<option>Select Address type</option>
-											<option>Office</option>
-											<option>Home</option>
-											<option>Commercial</option>
-
-										</select>
-									</div>
-								</div>
-								<button class="submit check_out btn">Delivery to this Address</button>
-							</div>
-						</div>
-					</form>
-					<div class="checkout-right-basket">
-						<a href="payment.php">Make a Payment
-							<span class="far fa-hand-point-right"></span>
-						</a>
-					</div>
-				</div>
+			<div class="checkout-right-basket">
+				<a href="payment.php">Make a Payment
+					<span class="far fa-hand-point-right"></span>
+				</a>
 			</div>
 		</div>
 	</div>
